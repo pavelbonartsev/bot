9f8e4f31aee41e8bf4fe3988f1d9dbd4821a6b4a
pavelbonartsev <150126230+pavelbonartsev@users.noreply.github.com>
2025-12-18 07:40:11 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,31 +1,36 @@
-token = '7206783181:AAEJZVB9YZXWnNTHPwi9HY1MInv9wBEc82w'
 import telebot
+from telebot import types
 
+token = '7206783181:AAEJZVB9YZXWnNTHPwi9HY1MInv9wBEc82w'
 bot = telebot.TeleBot(token)
 
-#@bot.message_handler(content_types=["text"])
-#def repeat_all_messages(message): # Название функции не играет никакой роли
-    #bot.send_message(message.chat.id, cezar(message.text))
-@bot.message_handler(content_types=['photo'])
-def photo(message):   
-     fileID = message.photo[-1].file_id   
-     file_info = bot.get_file(fileID)
-     downloaded_file = bot.download_file(file_info.file_path)
-     with open("image.jpg", 'wb') as new_file:
-         new_file.write(downloaded_file)
-     bot.send_photo(message.chat.id, downloaded_file)
+def create_main_keyboard():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn_help = types.KeyboardButton('/help')
+    btn_cezar = types.KeyboardButton('/cezar')
+    btn_start = types.KeyboardButton('/start')
+    markup.add(btn_start, btn_help, btn_cezar)
+    return markup
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,"Привет!")
+    markup = create_main_keyboard()
+    bot.send_message(message.chat.id, "Привет! Я бот с кнопками. Выберите действие:", reply_markup=markup)
+
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, "/start - старт работы с ботом\n/help - помощь\n/cezar - шифрование текста по шифру Цезаря\n")
+    help_text = (
+        "/start - старт работы с ботом\n"
+        "/help - помощь\n"
+        "/cezar - шифрование текста по шифру Цезаря\n\n"
+        "Также вы можете использовать кнопки ниже для навигации!"
+    )
+    bot.send_message(message.chat.id, help_text, reply_markup=create_main_keyboard())
+
 @bot.message_handler(commands=['cezar'])
 def byCezar(message):
-    bot.send_message(message.chat.id, "Введите текст...")
+    bot.send_message(message.chat.id, "Введите текст для шифрования Цезаря:", reply_markup=create_main_keyboard())
     bot.register_next_step_handler(message, cezar_2)
-def cezar_2(message):
-    bot.send_message(message.chat.id, cezar(message.text))
 def cezar(st):
     newSt = ""
     for i in st: 
@@ -36,9 +41,31 @@ def cezar(st):
         elif i == "Z":
             newSt += "A"
         elif i == "z":
-            newSt += "а"
+            newSt += "a"
         else:
             newSt += chr(ord(i) + 1)
     return newSt
+
+def cezar_2(message):
+    result = cezar(message.text)
+    bot.send_message(message.chat.id, f"Зашифрованный текст:\n{result}", reply_markup=create_main_keyboard())
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):   
+    fileID = message.photo[-1].file_id   
+    file_info = bot.get_file(fileID)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open("image.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+    bot.send_photo(message.chat.id, downloaded_file, reply_markup=create_main_keyboard())
+
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    if not message.text.startswith('/'):
+        bot.send_message(message.chat.id, 
+                        f"Вы написали: {message.text}\nИспользуйте кнопки или команды для работы с ботом.", 
+                        reply_markup=create_main_keyboard())
+
 if __name__ == '__main__':
-     bot.infinity_polling()
+    print("Бот запущен...")
+    bot.infinity_polling()
